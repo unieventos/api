838200981b1bcb20b26f92885fd9ad27cc49878f
MuriloROL <murilo.rl.dev@gmail.com>
2025-12-10 19:43:46 -0300

    refatorando registro de evento para incluir upload de foto

--- src/main/java/br/com/unisagrado/Unisagrado/unieventos/eventos/dto/CreateEventRecord.java
+++ src/main/java/br/com/unisagrado/Unisagrado/unieventos/eventos/dto/CreateEventRecord.java
@@ -1,11 +1,8 @@
 package br.com.unisagrado.Unisagrado.unieventos.eventos.dto;
 
-import org.springframework.web.multipart.MultipartFile;
-
 import java.time.LocalDate;
 
-
 public record CreateEventRecord(String nomeEvento, String descricao,
-	 LocalDate dateInicio, LocalDate dateFim, String categoria, MultipartFile foto) {
+	 LocalDate dateInicio, LocalDate dateFim, String categoria) {
 
 }
