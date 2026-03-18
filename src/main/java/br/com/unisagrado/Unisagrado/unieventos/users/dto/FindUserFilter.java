3fecc28838da30b36591cf1427d7a081b3346139
Otávio Pegorin <otaviopegorin@gmail.com>
2026-03-18 11:28:09 -0300

    mudando filtro para classe

--- src/main/java/br/com/unisagrado/Unisagrado/unieventos/users/dto/FindUserFilter.java
+++ src/main/java/br/com/unisagrado/Unisagrado/unieventos/users/dto/FindUserFilter.java
@@ -1,5 +1,42 @@
 package br.com.unisagrado.Unisagrado.unieventos.users.dto;
 
-public record FindUserFilter(String name, Boolean active) {
+public class FindUserFilter {
 
+	private String name;
+	private Boolean active;
+	
+	public FindUserFilter(Boolean active) {
+		this.name = "";
+		this.active = true;
+	}
+	
+	public FindUserFilter(String name) {
+		this.name = name;
+		this.active = true;
+	}
+	
+	public FindUserFilter() {
+		this.name = "";
+		this.active = true;
+	}
+	
+	public FindUserFilter(String name, Boolean active) {
+		this.name = name;
+		this.active = active;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Boolean getActive() {
+		return active;
+	}
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+	
+	
 }
