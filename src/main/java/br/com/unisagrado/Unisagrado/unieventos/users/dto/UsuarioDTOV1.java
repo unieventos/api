226f32f16868bd8354edc8e54e92d1ee9585c9ba
Otávio Pegorin <otaviopegorin@gmail.com>
2026-03-18 12:02:46 -0300

    adicionando login do usuario no retorno da api

--- src/main/java/br/com/unisagrado/Unisagrado/unieventos/users/dto/UsuarioDTOV1.java
+++ src/main/java/br/com/unisagrado/Unisagrado/unieventos/users/dto/UsuarioDTOV1.java
@@ -3,7 +3,7 @@ package br.com.unisagrado.Unisagrado.unieventos.users.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record UsuarioDTOV1(@NotBlank String id, @NotBlank String nome, @NotBlank String sobrenome,
+public record UsuarioDTOV1(@NotBlank String id, @NotBlank String nome,@NotBlank String login, @NotBlank String sobrenome,
 		@Email(message = "Email should be valid") String email, @NotBlank Long cursoId, boolean active) {
 
 }
