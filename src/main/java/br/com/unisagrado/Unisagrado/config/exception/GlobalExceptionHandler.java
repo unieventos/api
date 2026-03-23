3f7fcebf099cd7d563c7651b0a3b8a5d39f1f1a6
Otávio Pegorin <otaviopegorin@gmail.com>
2026-03-23 15:19:13 -0300

    adicionando logs no controller advice

--- src/main/java/br/com/unisagrado/Unisagrado/config/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/unisagrado/Unisagrado/config/exception/GlobalExceptionHandler.java
@@ -21,6 +21,7 @@ import br.com.unisagrado.Unisagrado.unieventos.eventos.exception.EventNotFoundEx
 import br.com.unisagrado.Unisagrado.unieventos.fotos.exception.FotoNotFoundException;
 import br.com.unisagrado.Unisagrado.unieventos.fotos.exception.FotosForTargetIdNotFoundException;
 import br.com.unisagrado.Unisagrado.unieventos.fotos.exception.GenericException;
+import br.com.unisagrado.Unisagrado.unieventos.fotos.service.FotoService;
 import br.com.unisagrado.Unisagrado.unieventos.storage.exception.SaveFileException;
 import br.com.unisagrado.Unisagrado.unieventos.users.exception.IllegarUserIdException;
 import br.com.unisagrado.Unisagrado.unieventos.users.exception.InvalidOperation;
@@ -31,36 +32,42 @@ import br.com.unisagrado.Unisagrado.unieventos.users.exception.UserNotFoundExcep
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 	
-	private Logger logger = LoggerFactory.getLogger(getClass());
-	
+	private static final Logger logger = LoggerFactory.getLogger(FotoService.class);
+
 
 	@ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorDTO> handleUserNotFoundException(UserNotFoundException e) {
+		logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorDTO(HttpStatus.NOT_FOUND.value(), "user_not_found", "Usuário não encontrado."));
     }
     
     @ExceptionHandler(TokenExpiredException.class)
     public ResponseEntity<ErrorDTO> handleTokenExpiredException(TokenExpiredException e) {
+    	logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorDTO(HttpStatus.UNAUTHORIZED.value(), "token_expired", "Token expirado."));
     }
     
     @ExceptionHandler(AccessTokenNotFound.class)
     public ResponseEntity<ErrorDTO> handleAccessTokenNotFound(AccessTokenNotFound e) {
+    	logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorDTO(HttpStatus.UNAUTHORIZED.value(), "token_not_found", "Token não encontrado."));
     }
     
     @ExceptionHandler(IllegarUserIdException.class)
     public ResponseEntity<ErrorDTO> handleIllegarUserIdException(IllegarUserIdException e) {
+    	logger.error(e.getMessage(),e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDTO(HttpStatus.BAD_REQUEST.value(), "invalid_user_id", "Id de usuário inválido."));
     }
     
     @ExceptionHandler(CursoNotFoundException.class)
     public ResponseEntity<ErrorDTO> handleCursoNotFoundException(CursoNotFoundException e) {
+    	logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDTO(HttpStatus.BAD_REQUEST.value(), "course_not_found", "Curso não encontrado."));
     }
     
     @ExceptionHandler(SendEmailException.class)
     public ResponseEntity<ErrorDTO> handleSendEmailException(SendEmailException e) {
+    	logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(), "send_email_error", "Não foi possível realizar o envio do email."));
     }
     
@@ -72,66 +79,79 @@ public class GlobalExceptionHandler {
     
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<ErrorDTO> handleBadCredentialsException(BadCredentialsException e) {
+    	logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDTO(HttpStatus.BAD_REQUEST.value(), "bad_credentials", "Usuário ou senha incorretos."));
     }
     
     @ExceptionHandler(RoleNotFoundException.class)
     public ResponseEntity<ErrorDTO> handleRoleNotFoundException(RoleNotFoundException e) {
+    	logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDTO(HttpStatus.BAD_REQUEST.value(), "role_not_found", "Role informada não foi encontrada."));
     }
     
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<ErrorDTO> handleAccessDeniedException(AccessDeniedException e) {
+    	logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ErrorDTO(HttpStatus.FORBIDDEN.value(), "access_denied", "Você não tem permissão para acessar esse recurso."));
     }
     
     @ExceptionHandler(UserAlreadyInactive.class)
     public ResponseEntity<ErrorDTO> handleUserAlreadyInactive(UserAlreadyInactive e) {
+    	logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDTO(HttpStatus.BAD_REQUEST.value(), "user_already_inactive", "Usuário já está inativo."));
     }
     
     @ExceptionHandler(CategoriaNotFoundException.class)
     public ResponseEntity<ErrorDTO> handleCategoriaNotFoundException(CategoriaNotFoundException e) {
+    	logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorDTO(HttpStatus.NOT_FOUND.value(), "categoria_not_found", "Não foi possível encontrar a categoria."));
     }
     
     @ExceptionHandler(FotoNotFoundException.class)
     public ResponseEntity<ErrorDTO> handleFotoNotFoundException(FotoNotFoundException e) {
+    	logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorDTO(HttpStatus.NOT_FOUND.value(), "foto_not_found", "Não foi possível encontrar a foto."));
     }
     
     @ExceptionHandler(EventNotFoundException.class)
     public ResponseEntity<ErrorDTO> handleEventNotFound(EventNotFoundException e) {
+    	logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorDTO(HttpStatus.NOT_FOUND.value(), "event_not_found", "Não foi possível encontrar o evento."));
     }
     
     @ExceptionHandler(SaveFileException.class)
     public ResponseEntity<ErrorDTO> handleSaveFileException(SaveFileException e) {
+    	logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(), "file_error", "Não foi possível salvar o seu arquivo."));
     }
     
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorDTO> handleIllegalArgumentException(IllegalArgumentException e) {
+    	logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDTO(HttpStatus.BAD_REQUEST.value(), "bad_request", e.getMessage()));
     }
     
     @ExceptionHandler(CourseAlreadyExists.class)
     public ResponseEntity<ErrorDTO> handleCourseAlreadyExists(CourseAlreadyExists e) {
+    	logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDTO(HttpStatus.BAD_REQUEST.value(), "course_error", "O curso informado já existe."));
     }
     
     @ExceptionHandler(GenericException.class)
     public ResponseEntity<ErrorDTO> handleGenericException(GenericException e) {
+    	logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDTO(HttpStatus.BAD_REQUEST.value(), "generic_exception", e.getMessage()));
     }
     
     @ExceptionHandler(FotosForTargetIdNotFoundException.class)
     public ResponseEntity<ErrorDTO> handleFotosForTargetIdNotFoundException(FotosForTargetIdNotFoundException e) {
+    	logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorDTO(HttpStatus.NOT_FOUND.value(), "fotos_not_found", e.getMessage()));
     }
     
     @ExceptionHandler(InvalidOperation.class)
     public ResponseEntity<ErrorDTO> handleInvalidOperation(InvalidOperation e) {
+    	logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDTO(HttpStatus.BAD_REQUEST.value(), "invalid_operation", e.getMessage()));
     }
     
